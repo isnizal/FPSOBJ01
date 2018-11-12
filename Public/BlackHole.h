21efec0c73b38e121080f0cfe5ac3fbb49266942
isnizal <31788317+isnizal@users.noreply.github.com>
2018-11-12 22:01:56 +0800

    UFUNCTION
    Understanding OverlapInnerSphere

--- Public/BlackHole.h
+++ Public/BlackHole.h
@@ -19,6 +19,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UFUNCTION()
 	void OverlapInnerSphere(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
 public:	
